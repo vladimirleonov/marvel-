e3472c32fe68d3851e171c42b41ff84cc990f189
vladimir <vladimirleonov789@gmail.com>
2022-01-02 00:15:55 +0300

    made part of the RandomChar component

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,35 +3,108 @@ import './randomChar.scss';
 import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
 
 class RandomChar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state=({
+            character: {
+                name: null,
+                description: null,
+                thumbnail: null,
+                homepage: null,
+                wiki: null
+            },
+            loading: false
+        })
+    }
 
     marvelService = new MarvelService();
 
+    async componentDidMount() {
+        this.setState({
+            ...this.state,
+            loading: true
+        })
+        const character = await this.marvelService.getCharacter();
+        this.setState({
+            ...this.state,
+            character: {
+                ...character
+            },
+            loading: false
+        })
+    }
+
+    getTotalWordsCount(str) {
+        let count = 0;
+        let words = str.split(" ");
+        for (let i = 0; i < words.length; i++) {
+            // inner loop -- do the count
+            if (words[i] != "") {
+                count += 1;
+            }
+        }
+
+        return (count);
+    }
+
+    getCroppedString(str, n) {
+        let wordsArr = str.split(" ");
+        console.log(wordsArr);
+
+        let croppedWordsArr = [];
+
+        wordsArr.forEach((item, i) => {
+            if(i <= n) {
+                croppedWordsArr.push(item);
+            }
+        })
+        console.log(croppedWordsArr);
+        const strNew =  croppedWordsArr.join(' ');
+        return strNew;
+    }
+
     render() {
-        this.marvelService.getCharacters()
-            .then(response => console.log(response))
-        this.marvelService.getCharacter()
-            .then(response => console.log(response))
+        let descriptionLength;
+        if(this.state.character.description) {
+            descriptionLength = this.getTotalWordsCount(this.state.character.description);
+        } else {
+            descriptionLength = 0
+        }
+
+        let description;
+        if(this.state.character.description && descriptionLength > 20) {
+            description = this.getCroppedString(this.state.character.description, 20) + '...'
+        } else {
+            description = this.state.character.description
+        }
+
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thor} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">Thor</p>
-                        <p className="randomchar__descr">
-                            As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href="#" className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href="#" className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
+                {
+                    this.state.loading ?
+                        <Spinner/>
+                        :
+                        <div className="randomchar__block">
+                            <img src={this.state.character.thumbnail} alt="Random character" className="randomchar__img"/>
+                            <div className="randomchar__info">
+                                <p className="randomchar__name">{this.state.character.name}</p>
+                                <p className="randomchar__descr">
+                                    {description}
+                                </p>
+                                <div className="randomchar__btns">
+                                    <a href={this.state.character.homepage} className="button button__main">
+                                        <div className="inner">homepage</div>
+                                    </a>
+                                    <a href={this.state.character.wiki} className="button button__secondary">
+                                        <div className="inner">Wiki</div>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                }
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,11 +1,24 @@
 export default class MarvelService {
-    async getCharacters () {
-        let response = await fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=c8f1b9d4937ef1256f1d96898ca20f7e')
+    getResource = async (url) => {
+        const response = await fetch(url);
         return await response.json();
     }
-    async getCharacter () {
-        let response = await fetch('https://gateway.marvel.com:443/v1/public/characters/1011031?apikey=c8f1b9d4937ef1256f1d96898ca20f7e')
-        return await response.json();
+    getCharacters = async () => {
+        let response = await fetch('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=300&apikey=c8f1b9d4937ef1256f1d96898ca20f7e')
+        return response.json();
     }
-    //1011031
+    getCharacter = async () => {
+        /*const response = await fetch('https://gateway.marvel.com:443/v1/public/characters/1011031?apikey=c8f1b9d4937ef1256f1d96898ca20f7e');*/
+        const response = await this.getResource('https://gateway.marvel.com:443/v1/public/characters/1011031?apikey=c8f1b9d4937ef1256f1d96898ca20f7e');
+        return {
+            name: response.data.results[0].name,
+            description: response.data.results[0].description,
+            thumbnail: response.data.results[0].thumbnail.path + '.' + response.data.results[0].thumbnail.extension,
+            homepage: response.data.results[0].urls[0].url,
+            wiki: response.data.results[0].urls[1].url
+        }
+    }
+
 }
+
+//1011031
