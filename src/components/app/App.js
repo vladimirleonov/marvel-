f244d830d41e13007829d5b484678f6a714593fc
vladimir <vladimirleonov789@gmail.com>
2022-03-20 22:02:53 +0300

    made lazy loading for all pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, Suspense} from "react";
 import {
     BrowserRouter,
     Routes,
     Route
 } from "react-router-dom";
-
+import Spinner from "../spinner/Spinner";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../../pages/index'
+const Page404 = React.lazy(() => import('../../pages/page404/Page404'));
+const ComicsPage = React.lazy(() => import('../../pages/comicsPage/ComicsPage'));
+const SingleComicPage = React.lazy(() => import('../../pages/singleComicPage/SingleComicPage'));
+const MainPage = React.lazy(() => import('../../pages/mainPage/MainPage'));
 
 const App = () => {
 
@@ -23,12 +19,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="comics" element={<ComicsPage/>}/>
-                        <Route path="comics/:comicId" element={<SingleComicPage/>} />
-                        <Route path="*" element={<Page404/>} />
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="comics" element={<ComicsPage/>}/>
+                            <Route path="comics/:comicId" element={<SingleComicPage/>} />
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </BrowserRouter>
